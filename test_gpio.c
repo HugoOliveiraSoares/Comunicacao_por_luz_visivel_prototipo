17a07e22438e911d2fbc251918684cce73ded6b2
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2023-09-30 12:28:31 -0300

    teste com interrupção

--- test_gpio.c
+++ test_gpio.c
@@ -3,6 +3,8 @@
 #include <wiringPi.h>
 
 #define PIN 3
+#define LED 4
+#define LEDA 6
 
 int cont = 0;
 int in = 0;
@@ -13,13 +15,18 @@ void contador(void);
 int main() {
 
   wiringPiSetup();
-  wiringPiISR(PIN, INT_EDGE_RISING, &contador);
+  wiringPiISR(PIN, INT_EDGE_RISING, contador);
   pinModeAlt(PIN, 6);
+  pinMode(LED, OUTPUT);
+  pinMode(LEDA, OUTPUT);
 
   while (true) {
-    // in = digitalRead(PIN);
-    // printf("%d\n", in);
-    // delay(500);
+    printf("Pisca led Amarelo\n");
+    waitForInterrupt(PIN, 1000);
+    digitalWrite(LEDA, HIGH);
+    delay(1000);
+    digitalWrite(LEDA, LOW);
+    delay(1000);
   }
 
   return 0;
@@ -28,5 +35,11 @@ int main() {
 void contador(void) {
 
   cont++;
-  printf("INTERRUPSSÃO: %d\n", cont);
+  printf("Pisca led Verde\n");
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
 }
