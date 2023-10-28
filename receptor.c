4111ea04bb5b2d7601bf070182acaf3e43051ce0
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2023-10-28 15:01:16 -0300

    corrige o print da mensagem inteira

--- receptor.c
+++ receptor.c
@@ -23,30 +23,27 @@ bool *get_byte() {
   return p;
 }
 
-void print_byte(bool *b) {
-  int a = 0;
+char byte_to_char(bool *b, int debug) {
+  int c = 0;
   for (int i = 0; i < 8; i++) {
-    a += *(b + i) * pow(2, 7 - i);
-    printf("%i", *(b + i));
+    c += *(b + i) * pow(2, 7 - i);
+    if (debug) {
+      printf("%i", *(b + i));
+    }
   }
-  printf(" - %c", a);
+  return (char)c;
+}
+
+void print_byte(bool *b) {
+  int c = 0;
+  c = byte_to_char(b, true);
+  printf(" - %c", c);
   printf("\n");
 }
 
 void print_msg(bool *b[5]) {
-  shift_left(b[0]);
-  for (int i = 1; i < 5; i++) {
-    shift_left(b[i]);
-    b[i - 1][7] = b[i][0];
-  }
-
-  int a = 0;
   for (int i = 0; i < 5; i++) {
-    a = 0;
-    for (int y = 0; y < 8; y++) {
-      a += b[i][y] * pow(2, 7 - y);
-    }
-    printf("%c", a);
+    printf("%c", byte_to_char(b[i], false));
   }
   printf("\n");
 }
